497970c3bf4ecd60fd68f5d622f8a34eb0bfdce1
rifqiapr <rifqiadiprasetya@gmail.com>
2023-10-21 22:23:33 +0700

    updated HomePage.dart

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,6 +1,4 @@
-import 'main.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -9,23 +7,22 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Sport Place'),
+          title: const Text('Sport Place'),
         ),
         body: ListView(
           children: <Widget>[
             Container(
-              margin: EdgeInsets.all(8),
-              child: Card(
+              margin: const EdgeInsets.all(8),
+              child: const Card(
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.all(Radius.circular(35))),
                 child: Column(
                   children: <Widget>[
                     ClipRRect(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(35),
-                        topRight: Radius.circular(35),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(24)
                       ),
-                      child: const Image(
+                      child: Image(
                           image: NetworkImage(
                               'https://asset.kompas.com/crops/eBpMGSf3pmecESwKqU0YMEvZdCs=/0x0:0x0/1200x800/data/photo/2022/02/16/620c993cc2f2b.jpeg'),
                           width: 360, height: 180, fit: BoxFit.fill),
@@ -50,18 +47,17 @@ class HomePage extends StatelessWidget {
             ),
 
             Container(
-              margin: EdgeInsets.all(8),
-              child: Card(
+              margin: const EdgeInsets.all(8),
+              child: const Card(
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.all(Radius.circular(35))),
                 child: Column(
                   children: <Widget>[
                     ClipRRect(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(35),
-                        topRight: Radius.circular(35),
+                      borderRadius: BorderRadius.all(
+                          Radius.circular(24)
                       ),
-                      child: const Image(
+                      child: Image(
                           image: NetworkImage(
                               'https://i0.wp.com/umsu.ac.id/berita/wp-content/uploads/2023/07/Sejarah-JIS-Sejak-Era-Foke-hingga-Anies-Baswedan.jpg?fit=275%2C183&ssl=1'),
                           width: 360, height: 170, fit: BoxFit.fill),
@@ -87,18 +83,17 @@ class HomePage extends StatelessWidget {
 
 
             Container(
-              margin: EdgeInsets.all(8),
-              child: Card(
+              margin: const EdgeInsets.all(8),
+              child: const Card(
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.all(Radius.circular(35))),
                 child: Column(
                   children: <Widget>[
                     ClipRRect(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(35),
-                        topRight: Radius.circular(35),
+                      borderRadius: BorderRadius.all(
+                          Radius.circular(24)
                       ),
-                      child: const Image(
+                      child: Image(
                           image: NetworkImage(
                               'https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt09800b929ac02d7d/60dd571422d24e38a390520e/6d4545d9b0f405797f9c991eeadad293a387d59c.jpg?auto=webp&format=pjpg&width=3840&quality=60'),
                           width: 360, height: 170, fit: BoxFit.fill),
